f2eb3744e7e3f46829b769367149db35d7070c36
Livin Anto Nellisserry <livin.nellisserry@gmail.com>
2021-06-26 20:36:40 +0530

    Crud Operations Added and Updated

--- src/main/java/com/api2/repository/ProductRepo.java
+++ src/main/java/com/api2/repository/ProductRepo.java
@@ -1,5 +1,7 @@
 package com.api2.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,5 @@ import com.api2.model.Product;
 @Repository
 public interface ProductRepo extends JpaRepository<Product, String>{
 
-	public Product findByProductId(String productId);
+	public Optional<Product> findByProductId(String productId);
 }
--- src/main/java/com/api2/service/ProductService.java
+++ src/main/java/com/api2/service/ProductService.java
@@ -5,5 +5,7 @@ import com.api2.model.Product;
 public interface ProductService {
 	public Product getProductById(String productId);
 	public Product addProduct(Product product);
+	public Product updateProduct(Product product);
+	public Integer deleteProduct(String productId);
 	
 }
